51cfd396ff073d75a75f0fe0635cd51ed86db874
mihkuno <caindayjoeninyo@gmail.com>
2024-03-01 16:34:33 +0800

    added user input

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+   public static void main(String[] args) throws Exception {
+      // object to get input from user
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter your username: ");
+      String username = sc.nextLine();
+      System.out.println("Username is: " + username);
+   }
+}
